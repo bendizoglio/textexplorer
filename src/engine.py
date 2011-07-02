5cf3aa0617cf5a69a1825f0c91aff99b5a7cd5fd
bendizoglio <bendizoglio@aol.com>
2011-07-02 10:59:09 -0400

    full navigation added with help and look commands

--- src/engine.py
+++ src/engine.py
@@ -1,7 +1,7 @@
 import csv
 
 class Inventory(dict):
-    def _init_(self):
+    def __init__(self):
         self.inventorylist = []
     def add_item(self,item):
         self.inventorylist.append(item)
@@ -11,8 +11,9 @@ class Inventory(dict):
 class Location():
     direction = ['n', 's', 'e', 'w', 'u', 'd','exit', 'look', 'inventory', 'search']
     
-    def _init_(self,name,desc):
-        self.m_locations = {'Town Square':'the center of town','North Wall':'the north wall', 'South Wall':'the south wall'}
+    def __init__(self,game_map):
+        self.game_map = game_map
+        print (self.game_map)
         
     def add_exit(self,direction,location):
         self.exits[direction] = location
@@ -21,8 +22,6 @@ class Location():
         return self.exits.get(direction)
     
     def resolve_location(self,choice):
-        
-        
         new_location = self.location
         
         if new_location:
@@ -32,10 +31,15 @@ class Location():
         else:
             print "you can't go that way."
         
+    def display_location(self,index):
+        print 'you find yourself at:'
+        print ((self.game_map[index])[0])
+    
+    def look_location(self,index):
+        print ((self.game_map[index])[1])
 
-
-class Player():
-    def _init_(self, location):
+class Player:
+    def __init__(self, location):
         self.location = location
         self.inventory = Inventory()
         
@@ -46,14 +50,23 @@ class Player():
     def display_inventory(self):
         print 'You carry:\n', self.inventory
         
-    def look_location(self):
-        print "You're in:\n", self.location
         
+    def get_location(self):
+        return (self.location)
+        
+    def set_location(self,location):
+        self.location = location
     
 
-class GamePlay(Location,Inventory):
-    game_map = {}
+class GamePlay():
+    def __init__(self):
+        self.game_map = {}
+        self.game_nav = {}
     
+    def get_game_map(self):
+        print self.game_map
+        return (self.game_map)
+        
     def loadGame(self):
         map_reader = csv.reader(open('../resources/map.txt', 'rU'), delimiter='\t', quotechar='|') 
         j=0
@@ -63,7 +76,7 @@ class GamePlay(Location,Inventory):
         for row in map_reader:
             if j == 25:
                 break
-            print (len(row))
+            #print (len(row))
             for i in range(len(row)):
                 index_list.append(25*j+i)
                 if row[i] == 'null':
@@ -73,66 +86,45 @@ class GamePlay(Location,Inventory):
                     desc_list.append(cell_list)
                 # print ('%d: %s' % (25*j+i,row[i]))
             j=j+1
-        print (len(index_list))
-        print (len(desc_list))
-        game_map = dict(zip(index_list,desc_list))
-        print (game_map)
+        #print (len(index_list))
+        #print (len(desc_list))
+        self.game_map = dict(zip(index_list,desc_list))
+        print (self.game_map)
         
         cell_nav = []
         current_nav = []
         
-        for current_index,v in game_map.items():
-            print (current_index)
-            print (game_map[current_index])
-            if (game_map[current_index]) != 'null':     
-                if (current_index - 25) >= 0 and (game_map[current_index - 25]) != 'null':
+        for current_index,v in self.game_map.items():
+            #print (current_index)
+            #print (game_map[current_index])
+            if (self.game_map[current_index]) != 'null':     
+                if (current_index - 25) >= 0 and (self.game_map[current_index - 25]) != 'null':
                     cell_nav.append('n')
-                if (current_index + 25) < 625 and (game_map[current_index + 25]) != 'null':
+                if (current_index + 25) < 625 and (self.game_map[current_index + 25]) != 'null':
                     cell_nav.append('s')
                 if current_index < 624:
-                    if ((current_index % 24) != 0 or (current_index == 0)) and (game_map[current_index + 1]) != 'null':
+                    if ((current_index % 24) != 0 or (current_index == 0)) and (self.game_map[current_index + 1]) != 'null':
                         cell_nav.append('e')
-                if ((current_index) % 24) >0 and (game_map[current_index - 1]) != 'null':
+                if ((current_index) % 24) >0 and (self.game_map[current_index - 1]) != 'null':
                     cell_nav.append('w')
                 
             current_nav.append(cell_nav)
             cell_nav = []
-            print (current_nav[current_index])
+            #print (current_nav[current_index])
             
-        print (index_list)    
-        print (len(current_nav))
-        print (len(index_list))
-        game_nav = dict(zip(index_list,current_nav))
-        print (game_nav)
+        #print (index_list)    
+        #print (len(current_nav))
+        #print (len(index_list))
+        self.game_nav = dict(zip(index_list,current_nav))
+        print (self.game_nav)
             
-
+    def display_navigation(self,index):
+        print (self.game_nav[index])
+    
     def play(self):
         self.help()
         self.player.look_location()
     
-    def playercommand(self,):
-        
-        _direction = Location()
-
-        while True:
-            commandx = raw_input('\n>')
-            try:
-                i = _direction.direction.index(commandx)
-            except ValueError:
-                i = -1
-
-            if i == 6:
-                break
-
-            elif i == 7:
-                Player.look_location()
-
-            elif i == 8:
-                self.player.display_inventory()
-            
-            """elif i == 9 :
-                location.inventorylist"""
-                
-        return(0)
+    
     
 
--- src/mainGame.py
+++ src/mainGame.py
@@ -5,19 +5,50 @@ from engine import Player
 from engine import GamePlay
 
 
-        
+
    
-Location()
-m_player = Player()
+m_player = Player(541)
 g_inventory = Inventory()
 game_play = GamePlay()
 
 game_play.loadGame()
+print (game_play.get_game_map())
+m_location = Location(game_play.get_game_map())
 
-runGame = 1
-while runGame == 1:
-    runGame = game_play.playercommand()
+while True:
+    m_location.display_location(m_player.get_location())
+    game_play.display_navigation(m_player.get_location())
+
+    _direction = m_location.direction
+
     
+    commandx = raw_input('\n>')
+    try:
+        i = _direction.index(commandx)
+    except ValueError:
+        i = -1
+    if commandx == 'n':
+        m_player.set_location(m_player.get_location()-25) 
+    if commandx == 's':
+        m_player.set_location(m_player.get_location()+25)
+    if commandx == 'e':
+        m_player.set_location(m_player.get_location()+1)
+    if commandx == "w":
+        m_player.set_location(m_player.get_location()-1)
+    if commandx == 'look':
+        m_location.look_location(m_player.get_location())
+    if commandx == 'help':
+        print (
+        '''list of commands:
+        n = NORTH
+        s = SOUTH
+        e = EAST
+        w = WEST
+        look = look around
+        search = search for items in the vicinity\n\n''')
+
+        
+
     
 
 
